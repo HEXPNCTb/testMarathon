1226fe34460cc39dc200b8ca20133f6d0a6c2c59
HEXPNCTb <HEXPNCTb@gmail.com>
2025-11-18 15:02:16 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terrabucket111111112222333333rnhjdfgdfjghfdo "
+    bucket       = "terrabucket111111112222333333rnhjdfgdfjghfdo"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
